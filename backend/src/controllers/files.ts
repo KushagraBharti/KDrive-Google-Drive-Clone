856cbc83111cb5d3f05b2bf9c74dcd6430378a28
Kushagra Bharti <65621922+KushagraBharti@users.noreply.github.com>
2025-09-12 04:01:30 -0500

    Store file paths and remove objects from storage

--- backend/src/controllers/files.ts
+++ backend/src/controllers/files.ts
@@ -1,4 +1,5 @@
 import prisma from '@/services/prisma';
+import { supabaseAdmin } from '@/services/supabase';
 
 export function getFiles(parentId: number, ownerId: string) {
   return prisma.file.findMany({
@@ -11,14 +12,17 @@ export function createFile(args: {
   name: string;
   size: number;
   url: string;
+  path: string;
   parentId: number;
   ownerId: string;
 }) {
   return prisma.file.create({ data: args });
 }
 
-export function deleteFile(id: number, ownerId: string) {
-  return prisma.file.delete({
+export async function deleteFile(id: number, ownerId: string) {
+  const file = await prisma.file.delete({
     where: { id, ownerId },
   });
+  await supabaseAdmin.storage.from('files').remove([file.path]);
+  return file;
 }
